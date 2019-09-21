85d71c397cffac0d83bf50828cdcf88e897fd6dd
SergioRosello <sergio-rosello@hotmail.com>
2019-09-22 00:14:59 +0200

    Changed to OOP

--- issueCreator.rb
+++ issueCreator.rb
@@ -9,7 +9,58 @@ require "net/http"
 # home/user/Documents/Wave/issues/Sister/iOS/1.0-20/issue.issuea
 # We check for Sister, Schools, Wola to determine project
 # We check for Android or iOS to determine platform
-#
+
+class Issue
+
+  # Initializes the class
+  def initialize(path)
+    @issue_path = path
+  end
+  
+  # Print error message
+  def abort_script
+    abort("Provide 1..N absolute filepaths in order to upload issues")  
+  end
+
+  # Returns the issue path
+  def path
+    @issue_path
+  end
+
+  # Returs the name in human readable form
+  def title
+    name = @issue_path.split(/\//)
+    # gets the last / separated part of the string
+    # divides the previous string where the "." appears
+    # replaces "_" with " "
+    name.last.split(/\./).first.gsub!('_', ' ')
+  end
+
+  # Reads the issue file and converts it into a string
+  def body
+    File.read(@issue_path)
+  end
+
+  # Add labels to issue.
+  # They will be proirity medium and type bug
+  def labels
+    ["Priority: Medium", "Type: Bug"]
+  end
+
+  # Generates the JSON object to send to Github
+  def generate_payload
+    # Parse the issue name
+    payload = {:title => title}
+    payload[:body] = body
+    payload[:labels] = labels
+    @payload = JSON.generate(payload)
+  end
+
+  def payload
+    @payload
+  end
+
+end
 
 # returns Github Project repository the issue has to go to
 def project(substring)
@@ -30,50 +81,17 @@ def project(substring)
   repo
 end
 
-# Print error message
-def abort_script
-  abort("Provide 1..N absolute filepaths in order to upload issues")  
-end
-
-# Returs the name in human readable form
-def issue_title(issue)
-  name = issue.split(/\//)
-  # gets the last / separated part of the string
-  # divides the previous string where the "." appears
-  # replaces "_" with " "
-  name.last.split(/\./).first.gsub!('_', ' ')
-end
-
-# Reads the issue file and converts it into a string
-def issue_body(issue)
-  File.read(issue)
-end
-
-# Add labels to issue.
-# They will be proirity medium and type bug
-def issue_labels
-  ["Priority: Medium", "Type: Bug"]
-end
-
-# Generates the JSON object to send to Github
-def generate_payload(issue)
-  # Parse the issue name
-  payload = {:title => issue_title(issue)}
-  payload[:body] = issue_body(issue)
-  payload[:labels] = issue_labels
-  payload
-end
-
-def upload(payload, issue)
+# Uploads the issue passed as arguments
+def upload(issue)
   # POST /repos/:owner/:repo/issues
-  uri = URI.parse("https://api.github.com/repos/WolaApplication/#{project(issue)}/issues")
+  uri = URI.parse("https://api.github.com/repos/WolaApplication/#{project(issue.path)}/issues")
   headers = {'Content.type': 'application/json',
              'Accept': 'application/vnd.github.v3+json'}
   # Create the HTTP objects
   response = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
     request = Net::HTTP::Post.new(uri.request_uri, headers)
     request.basic_auth(ENV['GITHUB_USERNAME'], ENV['GITHUB_TOKEN'])
-    request.body = payload
+    request.body = issue.payload
     request['Content-Type'] = 'application/json'
     request['Accept'] = 'application/vnd.github.v3+json'
 
@@ -86,6 +104,8 @@ end
 
 abort_script if ARGV.length == 0
 
-ARGV.each do |issue|
-  upload(JSON.generate(generate_payload(issue)), issue)
+ARGV.each do |path|
+  issue = Issue.new(path)
+  issue.generate_payload
+  upload(issue)
 end
